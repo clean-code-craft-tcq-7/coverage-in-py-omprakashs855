5a223bb0052256fd1e62b90f3c2384db379afd2f
Singh Om Prakash (MS/ESX8-INT-XC) <iom2kor@bosch.com>
2023-01-11 01:24:13 +0530

    adding more test cases

--- Test/typewise_alert.test.py
+++ Test/typewise_alert.test.py
@@ -22,9 +22,5 @@ class TypewiseTest(unittest.TestCase):
 
 
 
-  # def test_infers_breach_as_per_limits(self):
-  #   self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-
-
 if __name__ == '__main__':
   unittest.main()
